98c17cc3eeb5a3300e67b4e14e4879d50632799d
revanwirawan36-oss <revanwirawan36@gmail.com>
2025-10-25 22:06:43 +0700

    this is the first comit

---
+++ timeconv.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string pukul;
+    cin >> pukul;
+    
+    int jam= ((pukul[0]-'0')*10)+(pukul[1]-'0');
+    
+    if(pukul[8]=='A' && jam==12){
+        jam=0;
+    } else if(pukul[8]=='P' && jam!=12){
+        jam+=12;
+    }
+    cout << (jam <10 ? "0":"") << jam;
+    for(int i=2; i<8; i++ ){
+        cout << pukul[i];
+    }
+    
+    return 0;
+}
